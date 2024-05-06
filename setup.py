8f873ac0409836cc99a6150b613835293f160531
kzcyanide <snjt700@gmail.com>
2024-05-06 16:44:55 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Chest Cancer Detection Deep learning Project',
+    author='krishna soren',
+    license='MIT',
+)
